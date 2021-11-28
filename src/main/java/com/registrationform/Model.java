4c1bf2604f85a9a1330dd7b8ba6e51906fda4776
Cian Linehan <119301381@umail.ucc.ie>
2021-11-28 18:20:25 +0000

    Refactoring - In Model, removed redundant escapes in regEx pattern and moved pattern directly into compile function

--- src/main/java/com/registrationform/Model.java
+++ src/main/java/com/registrationform/Model.java
@@ -21,9 +21,8 @@ public class Model implements Contract.ModelInterface{
 
     @Override
     public void setEmail(String email) throws IllegalEmailFormatException {
-        String patternForEmail ="^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\" +
-                ".[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
-        Pattern emailPattern = Pattern.compile(patternForEmail);
+        Pattern emailPattern = Pattern.compile("^([a-zA-Z0-9_\\-.]+)@((\\[[0-9]{1,3}\\" +
+                ".[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$");
 
         if (!emailPattern.matcher(email).matches()){
             throw new IllegalEmailFormatException("Malformed email format");
