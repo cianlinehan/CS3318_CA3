8186b13b399fcb2bb8caa5e7072584d763d7b3ea
Cian Linehan <119301381@umail.ucc.ie>
2021-11-28 17:53:58 +0000

    Creates 5 failing tests for email in ModelTest test class

--- tests/com/registrationform/ModelTest.java
+++ tests/com/registrationform/ModelTest.java
@@ -2,6 +2,8 @@ package com.registrationform;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,12 +16,15 @@ class ModelTest {
         this.model =  new Model();
     }
 
+    // these are incorrect password and email formats which Model should throw exceptions to
     @Test
     void setPassword() {
 
     }
 
-    @Test
-    void setEmail() {
+    @ParameterizedTest
+    @ValueSource(strings = {"cianlinehan", "@gmail.com", "cianlinehan@gmail", "cianlinehangmail.com", "cianlinehan@gmailcom"})
+    void setEmail(String email) {
+        assertThrows(IllegalEmailFormatException.class, () -> model.setEmail(email));
     }
 }
